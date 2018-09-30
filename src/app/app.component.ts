f9e6d7b25a3ef22d72f3815ac08e04a78c75003b
Genaro Rico <grrico@hotmail.com>
2018-09-30 16:32:35 -0500

    add tab y botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Agular Material';
+  links = ['First', 'Second', 'Third'];
+  activeLink = this.links[0];
+  background = '';
+
+  toggleBackground() {
+    this.background = this.background ? '' : 'primary';
+  }
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -2,12 +2,16 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
+
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatListModule,MatCardModule, MatToolbarModule, BrowserModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatListModule,MatCardModule, MatToolbarModule, BrowserModule],
+  imports: [MatButtonModule, MatCheckboxModule,MatListModule,MatCardModule, MatToolbarModule, BrowserModule, MatIconModule, MatTabsModule],
+  exports: [MatButtonModule, MatCheckboxModule,MatListModule,MatCardModule, MatToolbarModule, BrowserModule, MatIconModule, MatTabsModule],
 })
 export class MaterialModule { }
